af931516e14f4e95753b31b256c5a1c3d7e25f61
greendb16 <greendb16@gmail.com>
2019-06-12 12:00:41 -0400

    first comit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        String myAnswer = keyboard.next();
+
+        firstInitial = keyboard.next ()
+        System.out.print(firstInitial + " " + lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city);
+    }
+}
